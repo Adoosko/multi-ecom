5f50ac0078348ff7b472c175c6cd3ebd81338a08
Adoosko <120666532+Adoosko@users.noreply.github.com>
2025-04-22 16:23:58 +0200

    02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Hello World</Button>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <Button variant={"elevated"}>Hello World</Button>
+        <Input></Input>
+        <Progress value={50}></Progress>
+        <Textarea value={"Iam text area"}></Textarea>
+        <Checkbox />
+      </div>
     </div>
   );
 }
